aff1d8abce35f501e4cb9c1eeaa38483f080f911
Zachary Juang <zachary822@me.com>
2021-11-14 21:53:01 -0500

    Proper syntax highlighting

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,10 +1,34 @@
 import Box from "@mui/material/Box";
+import { grey } from "@mui/material/colors";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
+import hljs from "highlight.js";
+import "highlight.js/styles/github.css";
 import ReactMarkdown from "markdown-to-jsx";
+import { Children, cloneElement, useRef } from "react";
 
 function MarkdownListItem(props: any) {
   return <Box component="li" sx={{ mt: 1, typography: "body1" }} {...props} />;
+}
+
+function Code({ children }: any) {
+  return (
+    <Box
+      component="pre"
+      sx={{ bgcolor: grey[100], borderRadius: 1, padding: 1 }}
+    >
+      {Children.map(children, (child) => {
+        const c = cloneElement(child, {
+          ref: useRef(),
+          style: { background: grey[100] },
+        });
+        if (c.ref.current) {
+          hljs.highlightElement(c.ref.current);
+        }
+        return c;
+      })}
+    </Box>
+  );
 }
 
 export const options = {
@@ -45,6 +69,9 @@ export const options = {
       props: {
         width: "100%",
       },
+    },
+    pre: {
+      component: Code,
     },
   },
 };
