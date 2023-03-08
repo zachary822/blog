196578e5c568421749f752cf88b0eb2882eba210
Zachary Juang <zachary822@me.com>
2023-03-08 00:10:01 -0500

    Update dependencies

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,5 +1,5 @@
 import { createTheme } from "@mui/material/styles";
-import { Roboto } from "@next/font/google";
+import { Roboto } from "next/font/google";
 
 export const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
