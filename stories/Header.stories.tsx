5e28f44471a9107f290002f97779a70db4859434
Zachary Juang <zachary822@me.com>
2023-04-05 23:59:18 -0400

    update storybook part 2

--- stories/Header.stories.tsx
+++ stories/Header.stories.tsx
@@ -6,19 +6,13 @@ import { Header } from "./Header";
 export default {
   title: "Example/Header",
   component: Header,
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: "fullscreen",
-  },
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  user: {
-    name: "Jane Doe",
-  },
+  user: {},
 };
 
 export const LoggedOut = Template.bind({});
--- stories/Header.tsx
+++ stories/Header.tsx
@@ -3,12 +3,8 @@ import React from "react";
 import { Button } from "./Button";
 import "./header.css";
 
-type User = {
-  name: string;
-};
-
 interface HeaderProps {
-  user?: User;
+  user?: {};
   onLogin: () => void;
   onLogout: () => void;
   onCreateAccount: () => void;
@@ -48,12 +44,7 @@ export const Header = ({
       </div>
       <div>
         {user ? (
-          <>
-            <span className="welcome">
-              Welcome, <b>{user.name}</b>!
-            </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
-          </>
+          <Button size="small" onClick={onLogout} label="Log out" />
         ) : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
